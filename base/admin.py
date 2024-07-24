32d5d1194f1c33cff60109b0d7f9c2af6e86efd7
yasir <mohammadyasirkmt@gmail.com>
2024-07-24 21:41:04 +0530

    updated

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from base.models import Room
+from base.models import Room, Topic, Message
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
 	list_display = ['id', 'name', 'desc', 'updated', 'created']
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name']
+
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+	list_display = ['id', 'user', 'room', 'body', 'updated', 'created']
